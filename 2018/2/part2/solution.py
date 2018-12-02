fa45616e391a28d24e526c945bee7f8dd83d1f15
Tadeo Kondrak <me@tadeo.ca>
2018-12-01 22:52:09 -0700

    Added solution 2 part 2

---
+++ 2018/2/part2/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import string
+
+with open("../input", "r") as file:
+    myInput = file.read().split('\n')
+    myInput = myInput[:-1] # remove last line, an empty one
+    letters = list(string.ascii_lowercase) # ['a', 'b', 'c'...]
+    for i in range(0, len(myInput)):
+        for j in myInput[:i]:
+            differences = 0
+            string1 = myInput[i]
+            string2 = j
+            for k in range(len(string1)):
+                if(string1[k] != string2[k]):
+                    differences += 1
+            if(differences == 1):
+                finalString = ""
+                for k in range(len(string1)):
+                    if(string1[k] == string2[k]):
+                        finalString += string1[k]
+                print(finalString)
