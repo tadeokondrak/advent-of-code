8db517a7c8f48b878e96e26b5b486a21cf602b7e
Tadeo Kondrak <me@tadeo.ca>
2018-12-02 23:23:37 -0700

    clean up day 3 solutions

--- 2018/3/part1/solution.py
+++ 2018/3/part1/solution.py
@@ -5,26 +5,30 @@ input_lines = open('../input', 'r').read().splitlines()
 
 grid = numpy.zeros((1000,1000))
 
-for i in input_lines:
-    split = i.split(' ')
+def parse_line(line: str) -> dict:
+    split = line.split(' ')
     claimid = int(split[0][1:])
     coords = tuple(int(i) for i in split[2][:-1].split(','))
     size = tuple(int(i) for i in split[3].split('x'))
 
-    x1 = coords[0]
-    y1 = coords[1]
-    x2 = coords[0] + size[0]
-    y2 = coords[1] + size[1]
+    return {'claimid': claimid,
+            'x1': coords[0],
+            'x2': coords[0] + size[0],
+            'y1': coords[1],
+            'y2': coords[1] + size[1]}
 
-    for j in range(x1, x2):
-        for k in range(y1, y2):
-            grid[j, k] += 1
+for i in input_lines:
+    parsed = parse_line(i)
+
+    for x in range(parsed['x1'], parsed['x2']):
+        for y in range(parsed['y1'], parsed['y2']):
+            grid[x, y] += 1
 
 multiClaimed = 0
 
-for i in range(0, 1000):
-    for j in range(0, 1000):
-        if grid[i, j] > 1:
+for x in range(0, 1000):
+    for y in range(0, 1000):
+        if grid[x, y] > 1:
             multiClaimed += 1
 
 print(multiClaimed)
--- 2018/3/part2/solution.py
+++ 2018/3/part2/solution.py
@@ -5,35 +5,34 @@ input_lines = open('../input', 'r').read().splitlines()
 
 grid = numpy.zeros((1000,1000))
 
-def parse_line(line: str) -> tuple:
+def parse_line(line: str) -> dict:
     split = line.split(' ')
     claimid = int(split[0][1:])
     coords = tuple(int(i) for i in split[2][:-1].split(','))
     size = tuple(int(i) for i in split[3].split('x'))
 
-    x1 = coords[0]
-    y1 = coords[1]
-    x2 = coords[0] + size[0]
-    y2 = coords[1] + size[1]
-
-    return ((claimid, ((x1, y1), (x2, y2))))
+    return {'claimid': claimid,
+            'x1': coords[0],
+            'x2': coords[0] + size[0],
+            'y1': coords[1],
+            'y2': coords[1] + size[1]}
 
 for i in input_lines:
     parsed = parse_line(i)
 
-    for j in range(parsed[1][0][0], parsed[1][1][0]):
-        for k in range(parsed[1][0][1], parsed[1][1][1]):
-            grid[j, k] += 1
+    for x in range(parsed['x1'], parsed['x2']):
+        for y in range(parsed['y1'], parsed['y2']):
+            grid[x, y] += 1
 
 for i in input_lines:
     uncontested = True
     parsed = parse_line(i)
 
-    for j in range(parsed[1][0][0], parsed[1][1][0]):
-        for k in range(parsed[1][0][1], parsed[1][1][1]):
-            if grid[j, k] > 1:
+    for x in range(parsed['x1'], parsed['x2']):
+        for y in range(parsed['y1'], parsed['y2']):
+            if grid[x, y] > 1:
                uncontested = False
 
     if uncontested is True:
-        print(parsed[0])
+        print(parsed['claimid'])
 
