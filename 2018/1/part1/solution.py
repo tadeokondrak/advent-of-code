dc6e6dd8b13ba4986167f257c4200cb3c831f3c5
Tadeo Kondrak <me@tadeo.ca>
2018-12-02 23:23:20 -0700

    rewrite day 1 solutions

--- 2018/1/part1/solution.py
+++ 2018/1/part1/solution.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-with open("../input", "r") as file:
-    myInput = file.read().split('\n')
-    myInput = myInput[:len(myInput)-1]
-    frequency = 0
-    for i in range(0, len(myInput)):
-        if(myInput[i][0:1] == "+"):
-            frequency = frequency + int(myInput[i][1:])
-        else:
-            frequency = frequency - int(myInput[i][1:])
-    print(frequency)
+input_lines = open('../input', 'r').read().splitlines()
+
+frequency = 0
+
+for i in input_lines:
+    if i[:1] == "+":
+        frequency = frequency + int(i[1:])
+    else:
+        frequency = frequency - int(i[1:])
+
+print(frequency)
--- 2018/1/part2/solution.py
+++ 2018/1/part2/solution.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-# SLOOOOOW but i couldn't come up with a better way until someone told me how they did it after
-with open("../input", "r") as file:
-    myInput = file.read().split('\n')
-    myInput = myInput[:len(myInput)-1]
-    array = [0]
-    frequency = 0
-    i = 0
-    while True:
-        if(i >= len(myInput)):
-            myInput = myInput + myInput
-        if(myInput[i][0:1] == "+"):
-            frequency = frequency + int(myInput[i][1:])
+
+input_lines = open('../input', 'r').read().splitlines()
+
+frequency = 0
+seen = set()
+
+found = False
+
+while found is False:
+    for i in input_lines:
+        if i[:1] == "+":
+            frequency = frequency + int(i[1:])
         else:
-            frequency = frequency - int(myInput[i][1:])
-        array.append(frequency)
-        if array[-1:][0] in array[0:-1]:
-            print(array[-1:][0])
+            frequency = frequency - int(i[1:])
+        if frequency in seen:
+            found = True
+            print(frequency)
             break
-        i += 1
+        seen.add(frequency)
+
