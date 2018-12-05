76553c3ca45ad0a9690ce76ce3b6a8a9503d2085
Tadeo Kondrak <me@tadeo.ca>
2018-12-04 21:20:43 -0700

    add rust solutions for day 3

---
+++ 2018/3/part1/solution.rs
@@ -0,0 +1,55 @@
+#[allow(dead_code)] // id isn't used in part 1
+struct Claim {
+    id: u32,
+    x1: u32,
+    x2: u32,
+    y1: u32,
+    y2: u32,
+}
+
+fn main() {
+    let input = include_str!("../input");
+    let mut grid = [[0u32; 1000]; 1000];
+    let mut claims = vec![];
+    for line in input.lines() {
+        let claim = parse_line(line);
+        claims.push(claim);
+    }
+    for claim in claims {
+        for x in claim.x1..claim.x2 {
+            for y in claim.y1..claim.y2 {
+                grid[x as usize][y as usize] += 1;
+            }
+        }
+    }
+    let mut multi_claimed: u32 = 0;
+    for x in 0..999 {
+        for y in 0..999 {
+            if grid[x as usize][y as usize] > 1 {
+                multi_claimed += 1;
+            }
+        }
+    }
+    println!("Square inches of fabric with overlapping claims: {}", multi_claimed)
+}
+
+fn parse_line(line: &str) -> Claim {
+    let split: Vec<&str> = line.split(' ').collect();
+    let claimid: u32 = split[0][1..].parse().unwrap();
+    let coords_str = &split[2][..&split[2].len()-1];
+    let coords: Vec<u32> = coords_str.split(',')
+        .map(|num| {
+            num.parse::<u32>().unwrap()
+        }).collect();
+    let size: Vec<u32> = split[3].split('x')
+        .map(|num| {
+            num.parse::<u32>().unwrap()
+        }).collect();
+    return Claim {
+        id: claimid,
+        x1: coords[0],
+        y1: coords[1],
+        x2: coords[0] + size[0],
+        y2: coords[1] + size[1]
+    };
+}
