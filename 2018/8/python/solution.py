43757708a06a494427636b85dd0682d8e5cecdec
Tadeo Kondrak <me@tadeo.ca>
2018-12-07 23:34:48 -0700

    slightly clean up day 8

--- 2018/8/python/solution.py
+++ 2018/8/python/solution.py
@@ -7,23 +7,26 @@ def get_length(node):
         length += get_length(child_node)
     return length
 
-
 def parse_node(input_list: list) -> dict:
+    child_node_count = input_list[0]
+    metadata_count = input_list[1]
+
     metadata_sum = 0
     value = 0
-    child_node_count = input_list[0]
     children = []
-    metadata_count = input_list[1]
-    rest = input_list[2:]
+
     offset = 2
     for i in range(child_node_count):
         child_node = parse_node(input_list[offset:])
         children.append(child_node)
         offset += get_length(child_node)
         metadata_sum += child_node['metadata_sum']
+
     metadata = input_list[offset:offset + metadata_count]
+
     for metadatum in metadata:
         metadata_sum += metadatum
+
     if child_node_count == 0:
         value = metadata_sum
     else:
