8a927dfdd25827e065be28c25442e5ad33d6f202
Tadeo Kondrak <me@tadeo.ca>
2019-12-04 19:10:34 -0700

    Day 4

--- 2019/04.rs
+++ 2019/04.rs
@@ -11,22 +11,17 @@ fn main() {
         .collect::<Vec<usize>>();
 
     let iter = (input[0]..=input[1])
-        .map(|n| n.to_string().bytes().map(|x| x - b'0').collect::<Vec<u8>>())
-        .filter(|n| !n.windows(2).any(|x| x[0] > x[1]));
+        .map(|n| n.to_string().into_bytes())
+        .filter(|n| n.windows(2).all(|x| x[0] <= x[1]));
 
-    let p1 = iter
-        .clone()
-        .filter(|n| n.windows(2).any(|x| x[0] == x[1]))
-        .count();
+    let p1 = iter.clone().filter(|n| n.windows(2).any(|x| x[0] == x[1]));
 
-    let p2 = iter
-        .filter(|n| {
-            n[0] == n[1] && n[1] != n[2]
-                || n.windows(4)
-                    .any(|x| x[0] != x[1] && x[1] == x[2] && x[2] != x[3])
-                || n[4] == n[5] && n[3] != n[4]
-        })
-        .count();
+    let p2 = iter.filter(|n| {
+        n[0] == n[1] && n[1] != n[2]
+            || n.windows(4)
+                .any(|x| x[0] != x[1] && x[1] == x[2] && x[2] != x[3])
+            || n[4] == n[5] && n[3] != n[4]
+    });
 
-    println!("Part 1: {}\nPart 2: {}", p1, p2);
+    println!("Part 1: {}\nPart 2: {}", p1.count(), p2.count());
 }
