69e1ceac63da5230d19f5d58615861e717c6f5aa
Tadeo Kondrak <me@tadeo.ca>
2019-12-04 21:45:17 -0700

    Day 2

--- 2019/02.rs
+++ 2019/02.rs
@@ -1,40 +1,30 @@
-use std::io::{stdin, Read};
-
-fn run(prog: &mut [usize], a: usize, b: usize) -> usize {
-    prog[1] = a;
-    prog[2] = b;
-    let mut pos = 0;
-    loop {
-        match prog[pos] {
-            op @ 1 | op @ 2 => {
-                let dst = prog[pos + 3];
-                prog[dst] = match op {
-                    1 => prog[prog[pos + 1]] + prog[prog[pos + 2]],
-                    2 => prog[prog[pos + 1]] * prog[prog[pos + 2]],
-                    _ => unreachable!(),
-                };
-                pos += 4;
-            }
-            99 => break prog[0],
-            _ => unreachable!(),
-        }
-    }
-}
+use std::io::stdin;
 
 fn main() {
     let mut prog = String::new();
-    stdin().read_to_string(&mut prog).unwrap();
-    let prog = prog
-        .trim()
-        .split(',')
-        .map(|x| x.parse().unwrap())
-        .collect::<Vec<usize>>();
+    stdin().read_line(&mut prog).unwrap();
+    let prog: Vec<usize> = prog.trim().split(',').map(|x| x.parse().unwrap()).collect();
 
-    let mut prog_buf = prog.clone();
     for a in 0..100 {
         for b in 0..100 {
-            prog_buf.copy_from_slice(&prog);
-            let result = run(&mut prog_buf, a, b);
+            let mut prog = prog.clone();
+            prog[1] = a;
+            prog[2] = b;
+            let result = (0..)
+                .step_by(4)
+                .find_map(|pos| match prog[pos] {
+                    op @ 1 | op @ 2 => {
+                        let dst = prog[pos + 3];
+                        prog[dst] = match op {
+                            1 => prog[prog[pos + 1]] + prog[prog[pos + 2]],
+                            2 => prog[prog[pos + 1]] * prog[prog[pos + 2]],
+                            _ => unreachable!(),
+                        };
+                        None
+                    }
+                    _ => Some(prog[0]),
+                })
+                .unwrap();
             if a == 12 && b == 2 {
                 println!("Part 1: {}", result);
             }
