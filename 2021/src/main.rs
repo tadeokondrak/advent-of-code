078543988e580aa01023d1fb6f6236d0b65bdae5
Tadeo Kondrak <me@tadeo.ca>
2021-12-01 21:52:28 -0700

    put solutions in reverse order, add tests

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -4,29 +4,19 @@ fn main() {
         input_dir: "input"
         challenges: [
             {
-                "2021-1-1": day1part1,
-                tests: []
+                "2021-1-2": day1part2,
+                tests: [
+                    { name: "1", input: "199\n200\n208\n210\n200\n207\n240\n269\n260\n263", output: "5" }
+                ]
             }
             {
-                "2021-1-2": day1part2,
-                tests: []
+                "2021-1-1": day1part1,
+                tests: [
+                    { name: "1", input: "199\n200\n208\n210\n200\n207\n240\n269\n260\n263", output: "7" }
+                ]
             }
         ]
     }
-}
-
-fn day1part1(input: &str) -> i32 {
-    let mut n = 0;
-    let mut last_val = None;
-    for val in input.lines().map(|x| x.parse::<i32>().unwrap()) {
-        if let Some(last_val) = last_val {
-            if val > last_val {
-                n += 1;
-            }
-        }
-        last_val = Some(val);
-    }
-    n
 }
 
 fn day1part2(input: &str) -> i32 {
@@ -41,6 +31,20 @@ fn day1part2(input: &str) -> i32 {
             }
         }
         last_sum = Some(sum);
+    }
+    n
+}
+
+fn day1part1(input: &str) -> i32 {
+    let mut n = 0;
+    let mut last_val = None;
+    for val in input.lines().map(|x| x.parse::<i32>().unwrap()) {
+        if let Some(last_val) = last_val {
+            if val > last_val {
+                n += 1;
+            }
+        }
+        last_val = Some(val);
     }
     n
 }
