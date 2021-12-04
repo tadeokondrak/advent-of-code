d5625d513c2a0f6267ec362b955f0d61ce2ed5db
Tadeo Kondrak <me@tadeo.ca>
2021-12-03 22:46:41 -0700

    day 4

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -9,6 +9,53 @@ fn main() {
         input_dir: "input"
         challenges: [
             {
+                "2021-4-2": day4part2,
+                tests: [
+                    { name: "1", input: "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7", output: "1924" }
+                ]
+            }            {
+                "2021-4-1": day4part1,
+                tests: [
+                    { name: "1", input: "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7", output: "4512" }
+                ]
+            }
+            {
                 "2021-3-2": day3part2,
                 tests: [
                     { name: "1", input: "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010", output: "230" }
@@ -45,6 +92,143 @@ fn main() {
             }
         ]
     }
+}
+
+#[derive(Clone)]
+struct Board {
+    nums: Vec<(u32, bool)>,
+}
+
+impl Board {
+    fn parse(s: &str) -> Self {
+        Self {
+            nums: s
+                .split(&[' ', '\n'])
+                .map(|x| x.trim_end())
+                .filter(|x| !x.is_empty())
+                .map(|x| (x.parse::<u32>().unwrap(), false))
+                .collect(),
+        }
+    }
+
+    fn call_number(&mut self, calling: u32) {
+        for (num, is_marked) in &mut self.nums {
+            if *num == calling {
+                *is_marked = true;
+            }
+        }
+    }
+
+    fn is_marked(&self, row: usize, col: usize) -> bool {
+        self.nums[row * 5 + col].1
+    }
+
+    fn check_rows(&self) -> bool {
+        'outer: for row in 0..5 {
+            for col in 0..5 {
+                if !self.is_marked(row, col) {
+                    continue 'outer;
+                }
+            }
+            return true;
+        }
+        false
+    }
+
+    fn check_cols(&self) -> bool {
+        'outer: for col in 0..5 {
+            for row in 0..5 {
+                if !self.is_marked(row, col) {
+                    continue 'outer;
+                }
+            }
+            return true;
+        }
+        false
+    }
+
+    fn score(&self, last_called: u32) -> Option<u32> {
+        if !self.check_rows() && !self.check_cols() {
+            return None;
+        }
+        let sum: u32 = self
+            .nums
+            .iter()
+            .filter(|(_, is_marked)| !*is_marked)
+            .map(|(x, _)| u32::from(*x))
+            .sum();
+        Some(sum * u32::from(last_called))
+    }
+}
+
+impl std::fmt::Debug for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in 0..5 {
+            for col in 0..5 {
+                let (num, marked) = self.nums[row * 5 + col];
+                if !marked {
+                    write!(f, "{:2}", num)?;
+                } else {
+                    write!(f, "\x1b[31;1m{:2}\x1b[0m", num)?;
+                }
+                if col != 4 {
+                    write!(f, " ")?;
+                }
+            }
+            if row != 5 {
+                write!(f, "\n")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn day4part2(input: &str) -> u32 {
+    let mut iter = input.split("\n\n");
+    let sequence = iter
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<u32>().unwrap());
+    let mut boards = Vec::new();
+    while let Some(board_str) = iter.next() {
+        boards.push(Some(Board::parse(board_str)));
+    }
+    let mut last_win = None;
+    for num in sequence {
+        for slot in &mut boards {
+            if let Some(board) = slot {
+                board.call_number(num);
+                if let Some(score) = board.score(num) {
+                    *slot = None;
+                    last_win = Some(score);
+                }
+            }
+        }
+    }
+    last_win.unwrap()
+}
+
+fn day4part1(input: &str) -> u32 {
+    let mut iter = input.split("\n\n");
+    let sequence = iter
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<u32>().unwrap());
+    let mut boards = Vec::new();
+    while let Some(board_str) = iter.next() {
+        boards.push(Board::parse(board_str));
+    }
+    for num in sequence {
+        for board in &mut boards {
+            board.call_number(num);
+            if let Some(score) = board.score(num) {
+                return score;
+            }
+        }
+    }
+    panic!()
 }
 
 fn ones_nones(count: usize, vals: &[u32]) -> ([u32; 32], [u32; 32]) {
