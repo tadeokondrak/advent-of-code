f52448962c410edd9a85db7ad150b6c575f86087
Tadeo Kondrak <me@tadeo.ca>
2021-12-05 22:30:40 -0700

    day 6

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -9,6 +9,18 @@ fn main() {
         input_dir: "input"
         challenges: [
             {
+                "2021-6-2": day6part2,
+                tests: [
+                    { name: "1", input: "3,4,3,1,2", output: "26984457539" }
+                ]
+            }
+            {
+                "2021-6-1": day6part1,
+                tests: [
+                    { name: "1", input: "3,4,3,1,2", output: "5934" }
+                ]
+            }
+            {
                 "2021-5-2": day5part2,
                 tests: [
                     { name: "1", input: "0,9 -> 5,9
@@ -123,6 +135,56 @@ fn main() {
             }
         ]
     }
+}
+
+fn day6(input: &str, iterations: usize) -> u64 {
+    let mut fishies = [0u64; 9];
+    for fishy in input.trim().split(",").map(|x| x.parse::<usize>().unwrap()) {
+        fishies[fishy] += 1;
+    }
+    for day in 0.. {
+        let sum = fishies.iter().sum::<u64>();
+        if day == iterations {
+            return sum;
+        }
+        let tmp = fishies[0];
+        for i in 0..8 {
+            fishies[i] = fishies[i + 1];
+        }
+        fishies[6] += tmp;
+        fishies[8] = tmp;
+    }
+    todo!()
+}
+
+fn day6part2(input: &str) -> u64 {
+    day6(input, 256)
+}
+
+fn day6part1(input: &str) -> usize {
+    let mut fishies = input
+        .trim()
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    for day in 1.. {
+        let mut new = 0;
+        for fishy in fishies.iter_mut() {
+            if *fishy == 0 {
+                *fishy = 6;
+                new += 1;
+            } else {
+                *fishy -= 1;
+            }
+        }
+        for _ in 0..new {
+            fishies.push(8);
+        }
+        if day == 80 {
+            return fishies.len();
+        }
+    }
+    panic!()
 }
 
 struct VentMap {
