17f020c63d558eb2d189c26d9eaa80db16de75e3
Tadeo Kondrak <me@tadeo.ca>
2022-12-05 22:22:45 -0700

    2022 Day 6

---
+++ 2022/06.rs
@@ -0,0 +1,54 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+fn solve(input: &str, size: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(size)
+        .enumerate()
+        .find(|(_, window)| {
+            window
+                .iter()
+                .enumerate()
+                .all(|(i, c)| !window[i + 1..].contains(&c))
+        })
+        .map(|(i, _)| i + size)
+        .unwrap()
+}
+
+fn part1(input: &str) -> usize {
+    solve(input, 4)
+}
+
+fn part2(input: &str) -> usize {
+    solve(input, 14)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
+}
