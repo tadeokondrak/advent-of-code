448f59dec15393f814f63f3e3b2a15d939b931be
Tadeo Kondrak <me@tadeo.ca>
2022-12-12 22:56:14 -0700

    2022 Day 13: clean up

--- 2022/13.rs
+++ 2022/13.rs
@@ -12,35 +12,41 @@ fn main() {
     println!("Part 2: {}", part2(&input));
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Eq, Clone)]
 #[serde(untagged)]
 enum Value {
     Int(i32),
     Array(Vec<Value>),
 }
 
-fn less(left: &Value, right: &Value) -> bool {
-    match (left, right) {
-        (Value::Int(x), Value::Int(y)) => x < y,
-        (&Value::Int(x), right @ Value::Array(_)) => {
-            less(&Value::Array(vec![Value::Int(x)]), right)
-        }
-        (left @ Value::Array(_), &Value::Int(y)) => less(left, &Value::Array(vec![Value::Int(y)])),
-        (Value::Array(x), Value::Array(y)) => {
-            let mut left_iter = x.iter();
-            let mut right_iter = y.iter();
-            loop {
-                match (left_iter.next(), right_iter.next()) {
-                    (None, None) => return false,
-                    (None, Some(_)) => return true,
-                    (Some(_), None) => return false,
-                    (Some(x), Some(y)) => {
-                        if less(x, y) {
-                            return true;
-                        }
-                        if less(y, x) {
-                            return false;
-                        }
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(<Self as Ord>::cmp(&self, other))
+    }
+}
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Value::Int(x), Value::Int(y)) => x.cmp(y),
+            (&Value::Int(x), right @ Value::Array(_)) => {
+                Value::Array(vec![Value::Int(x)]).cmp(&right)
+            }
+            (left @ Value::Array(_), &Value::Int(y)) => {
+                left.cmp(&Value::Array(vec![Value::Int(y)]))
+            }
+            (Value::Array(x), Value::Array(y)) => {
+                let mut left_iter = x.iter();
+                let mut right_iter = y.iter();
+                loop {
+                    match (left_iter.next(), right_iter.next()) {
+                        (None, None) => return Ordering::Equal,
+                        (None, Some(_)) => return Ordering::Less,
+                        (Some(_), None) => return Ordering::Greater,
+                        (Some(x), Some(y)) => match x.cmp(y) {
+                            ordering @ (Ordering::Less | Ordering::Greater) => return ordering,
+                            Ordering::Equal => {}
+                        },
                     }
                 }
             }
@@ -48,20 +54,26 @@ fn less(left: &Value, right: &Value) -> bool {
     }
 }
 
-fn part1(input: &str) -> i32 {
-    let mut count = 0;
-    for (i, packets) in input.split("\n\n").enumerate() {
-        let mut packets = packets.split("\n");
-        let left = serde_json::from_str::<Value>(packets.next().unwrap()).unwrap();
-        let right = serde_json::from_str::<Value>(packets.next().unwrap()).unwrap();
-        if less(&left, &right) {
-            count += i as i32 + 1;
-        }
-    }
-    count
+fn part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .enumerate()
+        .map(|(i, packets)| {
+            let mut packets = packets.split("\n");
+            let left = serde_json::from_str::<Value>(packets.next().unwrap()).unwrap();
+            let right = serde_json::from_str::<Value>(packets.next().unwrap()).unwrap();
+            (i, (left, right))
+        })
+        .filter(|(_, (a, b))| a < b)
+        .map(|(i, _)| i + 1)
+        .sum()
 }
 
 fn part2(input: &str) -> usize {
+    let distress = [
+        Value::Array(vec![Value::Array(vec![Value::Int(2)])]),
+        Value::Array(vec![Value::Array(vec![Value::Int(6)])]),
+    ];
     let mut packets = input
         .replace("\n\n", "\n")
         .split("\n")
@@ -69,38 +81,14 @@ fn part2(input: &str) -> usize {
         .filter(|(_, line)| !line.is_empty())
         .map(|(_, line)| serde_json::from_str::<Value>(line).unwrap())
         .collect::<Vec<_>>();
-    packets.push(Value::Array(vec![Value::Array(vec![Value::Int(2)])]));
-    packets.push(Value::Array(vec![Value::Array(vec![Value::Int(6)])]));
-    packets.sort_by(|a, b| {
-        if less(a, b) {
-            Ordering::Less
-        } else if less(b, a) {
-            Ordering::Greater
-        } else {
-            Ordering::Equal
-        }
-    });
-    let mut indices = packets
+    packets.extend(distress.iter().cloned());
+    packets.sort();
+    packets
         .iter()
         .enumerate()
-        .filter(|(_, v)| {
-            if let Value::Array(w) = v {
-                if w.len() != 1 {
-                    return false;
-                }
-                if let Value::Array(x) = &w[0] {
-                    if x.len() != 1 {
-                        return false;
-                    }
-                    if let Value::Int(2 | 6) = x[0] {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        })
-        .map(|(i, _)| i + 1);
-    indices.next().unwrap() * indices.next().unwrap()
+        .filter(|&(_, v)| distress.contains(v))
+        .map(|(i, _)| i + 1)
+        .product()
 }
 
 #[cfg(test)]
