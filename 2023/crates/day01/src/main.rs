43dea8967ea50aeec8bf5bead1f19a68b8cb7c78
Tadeo Kondrak <me@tadeo.ca>
2023-11-30 23:48:29 -0700

    2023 Day 1: remove allocations

--- 2023/crates/day01/src/main.rs
+++ 2023/crates/day01/src/main.rs
@@ -1,29 +1,72 @@
-use std::io::stdin;
+use std::io::{stdin, Read};
 
 fn main() {
-    let nums = [
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    println!("p1: {}", solve(&input, find_digit_simple));
+    println!("p2: {}", solve(&input, find_digit));
+}
+
+fn find_digit_simple(s: &str) -> Option<u32> {
+    s.chars().next().unwrap().to_digit(10)
+}
+
+fn find_digit_word(s: &str) -> Option<u32> {
+    [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let s = stdin()
+    ]
+    .iter()
+    .enumerate()
+    .find_map(|(i, word)| s.starts_with(word).then_some(i as u32 + 1))
+}
+
+fn find_digit(s: &str) -> Option<u32> {
+    find_digit_simple(s).or_else(|| find_digit_word(s))
+}
+
+fn solve(input: &str, find_digit: impl Fn(&str) -> Option<u32>) -> u32 {
+    input
         .lines()
-        .map(|x| x.unwrap())
         .map(|line| {
-            let mut digits = Vec::new();
-            for i in 0..line.len() {
-                let part = &line[i..];
-                for (i, num) in nums.iter().enumerate() {
-                    if part.starts_with(num) {
-                        digits.push(i + 1);
-                    }
-                }
-                for i in 0..=9 {
-                    if part.starts_with(&format!("{i}")) {
-                        digits.push(i);
-                    }
-                }
-            }
-            *digits.first().unwrap() * 10 + *digits.last().unwrap()
+            let mut forward = (0..line.len()).filter_map(|i| find_digit(&line[i..]));
+            let mut backward = (0..line.len()).rev().filter_map(|i| find_digit(&line[i..]));
+            10 * forward.next().unwrap() + backward.next().unwrap()
         })
-        .sum::<usize>();
-    println!("{s}");
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        assert_eq!(
+            solve(
+                "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+                find_digit_simple
+            ),
+            142
+        );
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(
+            solve(
+                "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+                find_digit
+            ),
+            281
+        );
+    }
 }
