38703b3ef3284a335115ed9ebb416140b8cc67da
Tadeo Kondrak <me@tadeo.ca>
2023-12-03 13:27:54 -0700

    2023 Day 3: Clean up

--- 2023/crates/day03/src/main.rs
+++ 2023/crates/day03/src/main.rs
@@ -21,7 +21,7 @@ struct Number {
     value: i32,
 }
 
-fn solve_p1(input: &str) -> i32 {
+fn parse(input: &str) -> (Vec<Symbol>, Vec<Number>) {
     let mut syms = Vec::new();
     let mut nums = Vec::new();
     for (y, line) in input.lines().enumerate() {
@@ -30,9 +30,7 @@ fn solve_p1(input: &str) -> i32 {
         let mut run = String::new();
         for c in line.trim().chars() {
             match c {
-                '0'..='9' => {
-                    run.push(c);
-                }
+                '0'..='9' => run.push(c),
                 _ => {
                     if !run.is_empty() {
                         nums.push(Number {
@@ -57,78 +55,37 @@ fn solve_p1(input: &str) -> i32 {
             })
         }
     }
+    (syms, nums)
+}
+
+impl Number {
+    fn is_adjacent_to(self, sym: Symbol) -> bool {
+        (self.x.0 - 1..=self.x.1 + 1).contains(&sym.x) && (self.y - 1..=self.y + 1).contains(&sym.y)
+    }
+}
+
+fn solve_p1(input: &str) -> i32 {
+    let (syms, nums) = parse(input);
     nums.iter()
-        .filter(|num| {
-            let is_adjacent_to_symbol = syms.iter().copied().any(|sym| {
-                let y = num.y;
-                (num.x.0..=num.x.1).any(|x| {
-                    (x.abs_diff(sym.x) == 0 && y.abs_diff(sym.y) == 1)
-                        || (x.abs_diff(sym.x) == 1 && y.abs_diff(sym.y) == 0)
-                        || (x.abs_diff(sym.x) == 1 && y.abs_diff(sym.y) == 1)
-                })
-            });
-            is_adjacent_to_symbol
-        })
+        .filter(|num| syms.iter().copied().any(|sym| num.is_adjacent_to(sym)))
         .map(|num| num.value)
         .sum()
 }
 
 fn solve_p2(input: &str) -> i32 {
-    let mut syms = Vec::new();
-    let mut nums = Vec::new();
-    for (y, line) in input.lines().enumerate() {
-        let y = y as i32;
-        let mut x = 0;
-        let mut run = String::new();
-        for c in line.trim().chars() {
-            match c {
-                '0'..='9' => {
-                    run.push(c);
-                }
-                _ => {
-                    if !run.is_empty() {
-                        nums.push(Number {
-                            x: (x - run.len() as i32, x - 1),
-                            y,
-                            value: run.parse().unwrap(),
-                        })
-                    }
-                    if c != '.' {
-                        syms.push(Symbol { x, y, c });
-                    }
-                    run.clear();
-                }
-            }
-            x += 1;
-        }
-        if !run.is_empty() {
-            nums.push(Number {
-                x: (x - run.len() as i32, x - 1),
-                y,
-                value: run.parse().unwrap(),
-            })
-        }
-    }
-    syms.iter().copied().filter(|sym| sym.c == '*').filter_map(|sym| {
-        let adjacent_nums = nums
-            .iter()
-            .copied()
-            .filter(|num| {
-                let y = num.y;
-                (num.x.0..=num.x.1).any(|x| {
-                    (x.abs_diff(sym.x) == 0 && y.abs_diff(sym.y) == 1)
-                        || (x.abs_diff(sym.x) == 1 && y.abs_diff(sym.y) == 0)
-                        || (x.abs_diff(sym.x) == 1 && y.abs_diff(sym.y) == 1)
-                })
-            })
-            .collect::<Vec<_>>();
-
-        if adjacent_nums.len() != 2 {
-            None
-        } else {
-            Some(adjacent_nums.iter().map(|num| num.value).product::<i32>())
-        }
-    }).sum()
+    let (syms, nums) = parse(input);
+    syms.iter()
+        .copied()
+        .filter(|sym| sym.c == '*')
+        .filter_map(|sym| {
+            let adjacent_nums = nums
+                .iter()
+                .copied()
+                .filter(|num| num.is_adjacent_to(sym))
+                .collect::<Vec<_>>();
+            (adjacent_nums.len() == 2).then(|| adjacent_nums[0].value * adjacent_nums[1].value)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -170,6 +127,6 @@ mod tests {
 .664.598..",
             ),
             467835
-        );;
+        );
     }
 }
