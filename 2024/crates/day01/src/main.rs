c0db44748e34cc39a8aad729472b012dc217ba1d
Tadeo Kondrak <me@tadeo.ca>
2024-12-02 10:29:52 -0700

    2024 Day 1: Add benchmark

--- 2024/crates/day01/src/main.rs
+++ 2024/crates/day01/src/main.rs
@@ -1,37 +1,39 @@
+#![feature(test)]
 use std::io::{self, Read};
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    let parsed = parse(&input);
+    println!("Part 1: {}", part1(&parsed));
+    println!("Part 2: {}", part2(&parsed));
 }
 
 fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
-    let l = input
+    let mut l: Vec<u32> = input
         .lines()
         .map(|line| line.split("   ").next().unwrap().parse().unwrap())
         .collect();
-    let r = input
+    let mut r: Vec<u32> = input
         .lines()
         .map(|line| line.split("   ").skip(1).next().unwrap().parse().unwrap())
         .collect();
+    l.sort();
+    r.sort();
     (l, r)
 }
 
-fn part1(input: &str) -> u32 {
-    let (mut l, mut r) = parse(input);
-    l.sort();
-    r.sort();
-    l.into_iter()
-        .zip(r.into_iter())
+fn part1((l, r): &(Vec<u32>, Vec<u32>)) -> u32 {
+    l.iter()
+        .copied()
+        .zip(r.iter().copied())
         .map(|(lv, rv)| lv.abs_diff(rv))
         .sum()
 }
 
-fn part2(input: &str) -> u32 {
-    let (l, r) = parse(input);
-    l.into_iter()
+fn part2((l, r): &(Vec<u32>, Vec<u32>)) -> u32 {
+    l.iter()
+        .copied()
         .map(|lv| {
             let freq = r.iter().copied().filter(|&rv| rv == lv).count() as u32;
             lv * freq
@@ -41,7 +43,12 @@ fn part2(input: &str) -> u32 {
 
 #[cfg(test)]
 mod tests {
+    extern crate test;
+
     use super::*;
+    use std::hint::black_box;
+    use test::Bencher;
+
     const TEST_INPUT: &str = "3   4
 4   3
 2   5
@@ -51,11 +58,31 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 11);
+        assert_eq!(part1(&parse(TEST_INPUT)), 11);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 31);
+        assert_eq!(part2(&parse(TEST_INPUT)), 31);
+    }
+
+    #[bench]
+    fn parsing(b: &mut Bencher) {
+        let input = std::fs::read_to_string("input").unwrap();
+        b.iter(|| drop(parse(&input)));
+    }
+
+    #[bench]
+    fn real_p1(b: &mut Bencher) {
+        let input = std::fs::read_to_string("input").unwrap();
+        let parsed = parse(&input);
+        b.iter(|| assert_eq!(part1(black_box(&parsed)), 1320851));
+    }
+
+    #[bench]
+    fn real_p2(b: &mut Bencher) {
+        let input = std::fs::read_to_string("input").unwrap();
+        let parsed = parse(&input);
+        b.iter(|| assert_eq!(part2(black_box(&parsed)), 26859182));
     }
 }
