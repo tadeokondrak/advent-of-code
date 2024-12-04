fe04e53b7420f5cd992727a91864db4fc324ee72
Tadeo Kondrak <me@tadeo.ca>
2024-12-03 22:33:36 -0700

    2024 Day 4: Cleanup

--- 2024/crates/day04/src/main.rs
+++ 2024/crates/day04/src/main.rs
@@ -8,7 +8,33 @@ fn main() {
     println!("Part 2: {}", part2(&input));
 }
 
-fn part1(input: &str) -> i32 {
+fn search(
+    grid: &[char],
+    width: i32,
+    height: i32,
+    dx: i32,
+    dy: i32,
+    x: i32,
+    y: i32,
+    word: &str,
+) -> bool {
+    if x < 0 || y < 0 || x >= width || y >= height {
+        return false;
+    }
+    if word.is_empty() {
+        return true;
+    }
+    let first_char = word.chars().next().unwrap();
+    if grid[(y * width + x) as usize] != first_char {
+        return false;
+    }
+    if word.len() == 1 {
+        return true;
+    }
+    search(grid, width, height, dx, dy, x + dx, y + dy, &word[1..])
+}
+
+fn parse(input: &str) -> (Vec<char>, i32, i32) {
     let width = input.lines().next().unwrap().len() as i32;
     let height = input.lines().count() as i32;
     let mut grid = vec!['\0'; (width * height) as usize];
@@ -19,206 +45,42 @@ fn part1(input: &str) -> i32 {
             grid[(y * width + x) as usize] = ch;
         }
     }
+    (grid, width, height)
+}
 
-    fn search(
-        grid: &[char],
-        width: i32,
-        height: i32,
-        dx: i32,
-        dy: i32,
-        x: i32,
-        y: i32,
-        word: &str,
-    ) -> bool {
-        if x < 0 || y < 0 || x >= width || y >= height {
-            return false;
-        }
-        if word.is_empty() {
-            return true;
-        }
-        //dbg!(width, height, dx, dy, x, y, word);
-
-        let first_char = word.chars().next().unwrap();
-        if grid[(y * width + x) as usize] != first_char {
-            return false;
-        }
-        if word.len() == 1 {
-            return true;
-        }
-        search(grid, width, height, dx, dy, x + dx, y + dy, &word[1..])
-    }
-
-    fn show(
-        grid: &[char],
-        full_new_grid: &mut [char],
-        width: i32,
-        height: i32,
-        dx: i32,
-        dy: i32,
-        mut x: i32,
-        mut y: i32,
-        word: &str,
-    ) {
-        let mut new_grid = grid.iter().copied().map(|x| '.').collect::<Vec<char>>();
-        for _ in 0..word.len() {
-            new_grid[(y * width + x) as usize] =
-                grid[(y * width + x) as usize].to_ascii_uppercase();
-            full_new_grid[(y * width + x) as usize] =
-                grid[(y * width + x) as usize].to_ascii_uppercase();
-            x += dx;
-            y += dy;
-        }
-        for y in 0..height {
-            for x in 0..width {
-                //eprint!("{}", new_grid[(y * width + x) as usize]);
-            }
-            //eprintln!();
-        }
-        //eprintln!();
-    }
-
-    const WORD: &str = "XMAS";
-
+fn part1(input: &str) -> i32 {
+    let (grid, width, height) = parse(input);
     let mut count = 0;
-    let mut full_new_grid = grid.iter().copied().map(|x| '.').collect::<Vec<char>>();
     for y in 0..height {
         for x in 0..width {
-            let mut add = |grid: &[char], width, height, dx, dy, x, y, word| {
-                //eprintln!("{:?}", (x, y, dx, dy));
-                if search(&grid, width, height, dx, dy, x, y, word) {
-                    show(&grid, &mut full_new_grid, width, height, dx, dy, x, y, word);
-                    count += 1;
-                }
-            };
-            add(&grid, width, height, -1, -1, x, y, WORD);
-            add(&grid, width, height, -1, 0, x, y, WORD);
-            add(&grid, width, height, -1, 1, x, y, WORD);
-            add(&grid, width, height, 0, -1, x, y, WORD);
-            add(&grid, width, height, 0, 1, x, y, WORD);
-            add(&grid, width, height, 1, -1, x, y, WORD);
-            add(&grid, width, height, 1, 0, x, y, WORD);
-            add(&grid, width, height, 1, 1, x, y, WORD);
+            count += search(&grid, width, height, -1, -1, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, -1, 0, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, -1, 1, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, 0, -1, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, 0, 1, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, 1, -1, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, 1, 0, x, y, "XMAS") as i32;
+            count += search(&grid, width, height, 1, 1, x, y, "XMAS") as i32;
         }
     }
-
-    //assert!(search(&grid, width, height, -1, -1, 4, 9, "XMAS"));
-
-    for y in 0..height {
-        for x in 0..width {
-            //eprint!("{}", full_new_grid[(y * width + x) as usize]);
-        }
-        //eprintln!();
-    }
-    //eprintln!();
-
     count
 }
 
-fn part2(input: &str) -> i32 {
-    let width = input.lines().next().unwrap().len() as i32;
-    let height = input.lines().count() as i32;
-    let mut grid = vec!['\0'; (width * height) as usize];
-    for (y, line) in input.lines().enumerate() {
-        let y = y as i32;
-        for (x, ch) in line.chars().enumerate() {
-            let x = x as i32;
-            grid[(y * width + x) as usize] = ch;
-        }
-    }
-
-    fn search(
-        grid: &[char],
-        width: i32,
-        height: i32,
-        dx: i32,
-        dy: i32,
-        x: i32,
-        y: i32,
-        word: &str,
-    ) -> bool {
-        if x < 0 || y < 0 || x >= width || y >= height {
-            return false;
-        }
-        if word.is_empty() {
-            return true;
-        }
-        //dbg!(width, height, dx, dy, x, y, word);
-
-        let first_char = word.chars().next().unwrap();
-        if grid[(y * width + x) as usize] != first_char {
-            return false;
-        }
-        if word.len() == 1 {
-            return true;
-        }
-        search(grid, width, height, dx, dy, x + dx, y + dy, &word[1..])
-    }
-
-    fn show(
-        grid: &[char],
-        full_new_grid: &mut [char],
-        width: i32,
-        height: i32,
-        dx: i32,
-        dy: i32,
-        mut x: i32,
-        mut y: i32,
-        word: &str,
-    ) {
-        let mut new_grid = grid.iter().copied().map(|x| '.').collect::<Vec<char>>();
-        for _ in 0..word.len() {
-            new_grid[(y * width + x) as usize] =
-                grid[(y * width + x) as usize].to_ascii_uppercase();
-            full_new_grid[(y * width + x) as usize] =
-                grid[(y * width + x) as usize].to_ascii_uppercase();
-            x += dx;
-            y += dy;
-        }
-        for y in 0..height {
-            for x in 0..width {
-                eprint!("{}", new_grid[(y * width + x) as usize]);
-            }
-            eprintln!();
-        }
-        eprintln!();
-    }
-
-    const WORD: &str = "MAS";
-
-    let mut count = 0;
-    let mut full_new_grid = grid.iter().copied().map(|x| '.').collect::<Vec<char>>();
-    for y in 0..height {
-        for x in 0..width {
-            let mut check = |grid: &[char], width, height, dx, dy, x, y, word| -> bool {
-                eprintln!("{:?}", (x, y, dx, dy));
-                if search(&grid, width, height, dx, dy, x, y, word) {
-                    //show(&grid, &mut full_new_grid, width, height, dx, dy, x, y, word);
-                    true
-                } else {
-                    false
-                }
-            };
-            let a = check(&grid, width, height, 1, 1, x - 1, y - 1, "MAS")
-                || check(&grid, width, height, 1, 1, x - 1, y - 1, "SAM");
-            let b = check(&grid, width, height, -1, 1, x + 1, y - 1, "MAS")
-                || check(&grid, width, height, -1, 1, x + 1, y - 1, "SAM");
-            if a && b {
-                count += 1;
-            }
-        }
-    }
-
-    //assert!(search(&grid, width, height, -1, -1, 4, 9, "XMAS"));
-
-    for y in 0..height {
-        for x in 0..width {
-            eprint!("{}", full_new_grid[(y * width + x) as usize]);
-        }
-        eprintln!();
-    }
-    eprintln!();
-
-    count
+fn part2(input: &str) -> usize {
+    let (grid, width, height) = parse(input);
+    (0..height)
+        .map(|y| {
+            (0..width)
+                .filter(|x| {
+                    let left = search(&grid, width, height, 1, 1, x - 1, y - 1, "MAS")
+                        || search(&grid, width, height, 1, 1, x - 1, y - 1, "SAM");
+                    let right = search(&grid, width, height, -1, 1, x + 1, y - 1, "MAS")
+                        || search(&grid, width, height, -1, 1, x + 1, y - 1, "SAM");
+                    left && right
+                })
+                .count()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -248,5 +110,17 @@ MXMXAXMASX";
     #[test]
     fn test_part2() {
         assert_eq!(part2(TEST_INPUT), 9);
+    }
+
+    #[bench]
+    fn real_p1(b: &mut Bencher) {
+        let input = std::fs::read_to_string("input").unwrap();
+        b.iter(|| assert_eq!(part1(black_box(&input)), 2344));
+    }
+
+    #[bench]
+    fn real_p2(b: &mut Bencher) {
+        let input = std::fs::read_to_string("input").unwrap();
+        b.iter(|| assert_eq!(part2(black_box(&input)), 1815));
     }
 }
